4711318a31a3cb7757060f3bd09983b80f2efcdf
ccsum19 <suminyj@gmail.com>
2024-05-28 05:36:07 +0900

    fix: user-call-image upload

--- js/user/main.js
+++ js/user/main.js
@@ -13,18 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
     callIcon.addEventListener('click', () => {
         if (!callStarted) {
             callStarted = true;
-            callIcon.src = '../../../asset/call_end.png'; // 빨간색 아이콘으로 변경
+            callIcon.src = '../../asset/call_end.png'; // 빨간색 아이콘으로 변경
             scheduleButton.style.display = 'none';
             interval = setInterval(() => {
                 timer++;
-                counter.textContent = `${timer}`;
+                counter.textContent = `00:${timer}`;
                 if (timer === 5) {
                     prompt.textContent = '오늘 기분은 어때요?';
-                    characterImg.src = '../../../asset/armi_mid.png'; // 소녀 아이콘 변경
+                    characterImg.src = '../../asset/armi_mid.png'; // 소녀 아이콘 변경
                 } else if (timer === 15) {
                     prompt.textContent = '통화가 완료되었습니다!';
-                    characterImg.src = '../../../asset/armi_end.png'; // 다시 초록색 아이콘으로 변경
-                    callIcon.src = '../../../asset/armi_call.png'; 
+                    characterImg.src = '../../asset/armi_end.png'; // 다시 초록색 아이콘으로 변경
+                    callIcon.src = '../../asset/armi_call.png'; 
                     scheduleButton.style.display = 'block'; // 일정 확인하기 버튼 다시 표시
                     clearInterval(interval);
                 }
